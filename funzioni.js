1cf241c6e843d22e59b4d5ad0046b8c675f82d9a
Arela-Cristina <instagrandine@gmail.com>
2024-06-13 17:39:32 +0200

    Add function gameStructure

---
+++ funzioni.js
@@ -0,0 +1,7 @@
+// funzione per creare i cuadratini
+
+function gameStructure(tag, classToAdd) {
+    const box = document.createElement(tag);
+    box, classListe.add(classToAdd);
+    return box;
+}
