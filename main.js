6bdf9c0fadad858fce54c180d17fa3ad81684321
Arela-Cristina <instagrandine@gmail.com>
2024-06-13 17:07:16 +0200

    Fix myButton innerHTML

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
 let containerMinato = document.getElementById('campoMinato');
-console.log(containerMinato);
+// console.log(containerMinato);
 
 let myButton = document.getElementById('myButton');
-console.log(myButton);
+// console.log(myButton);
 
-//aggiungiamo evento al bottone
+// //aggiungiamo evento al bottone
 myButton.addEventListener('click', function () {
-    containerMinato.classList.add('active');
+    containerMinato.innerHTML = '';
+
+    //creiamo 100 celle con il ciclo for
+    for (i = 1; i <= 100; i++) {
+        // console.log(i);
+
+
+        //creiamo un elemento div che rappresentara ogni cella
+        let campoSingolo = document.createElement('div');
+        // console.log(campoSingolo);
+        //ogni cella ha un numero del 1 al 100
+        campoSingolo.innerHTML += i;
+
+
+        //assegniamo la clasesquare al nostro div
+        campoSingolo.classList.add('square');
+        // console.log(campoSingolo);
+
+        //appendiamo ogni campo singolo nel caontainerMinato.
+        containerMinato.append(campoSingolo);
+
+        //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
+        campoSingolo.addEventListener('click', function () {
+            campoSingolo.classList.toggle('squareClick');
+            //ed emetto un messaggio in console con il numero  della cella cliccata.
+            console.log('Cella clicata', this.innerHTML);
+        });
+    }
 });
 
-//creiamo 100 celle con il ciclo for
-for (i = 1; i <= 100; i++) {
-    console.log(i);
-
-
-    //creiamo un elemento div che rappresentara ogni cella
-    let campoSingolo = document.createElement('div');
-    // console.log(campoSingolo);
-    //ogni cella ha un numero del 1 al 100
-    campoSingolo.innerHTML += i;
-
-
-    //assegniamo la clasesquare al nostro div
-    campoSingolo.classList.add('square');
-    // console.log(campoSingolo);
-
-    //appendiamo ogni campo singolo nel caontainerMinato.
-    containerMinato.append(campoSingolo);
-
-    //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
-    campoSingolo.addEventListener('click', function () {
-        campoSingolo.classList.toggle('squareClick');
-        //ed emetto un messaggio in console con il numero  della cella cliccata.
-        console.log('Cella clicata', this.innerHTML);
-    });
-}
 
 
 
