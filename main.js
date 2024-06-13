7e3571f7c2d02dc639874a301388bb6aa7c79cd6
Arela-Cristina <instagrandine@gmail.com>
2024-06-13 15:39:16 +0200

    Add cycle 'for'  to created 100 squares. Then created a new div to add 'square' properties. Then create an eventListener  in each square.

---
+++ main.js
@@ -0,0 +1,52 @@
+let containerMinato = document.getElementById('campoMinato');
+console.log(containerMinato);
+
+
+//creiamo 100 celle con il ciclo for
+for (i = 1; i <= 100; i++) {
+    console.log(i);
+
+
+    //creiamo un elemento div che rappresentara ogni cella
+    let campoSingolo = document.createElement('div');
+    // console.log(campoSingolo);
+    //ogni cella ha un numero del 1 al 100
+    campoSingolo.innerHTML += i;
+
+
+    //assegniamo la clasesquare al nostro div
+    campoSingolo.classList.add('square');
+    // console.log(campoSingolo);
+
+    //appendiamo ogni campo singolo nel caontainerMinato.
+    containerMinato.append(campoSingolo);
+
+    //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
+    campoSingolo.addEventListener('click', function () {
+        campoSingolo.classList.toggle('squareClick');
+        //ed emetto un messaggio in console con il numero  della cella cliccata.
+        console.log('Cella clicata', this.innerHTML);
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
